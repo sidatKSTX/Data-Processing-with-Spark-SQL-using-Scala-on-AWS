947f224209b914fae712a555000a59a7101f2443
Ubuntu <ubuntu@ip-10-0-12-62.us-west-1.compute.internal>
2025-11-12 16:08:48 +0000

    added code

---
+++ EC2.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-2a"
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+# Associate route table with subnet
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_sg"
+  description = "Allow all inbound and outbound traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all_sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "spark_ec2" {
+  ami                         = "ami-04985531f48a27ae7"
+  instance_type               = "t2.xlarge"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.allow_all.id]
+  associate_public_ip_address = true
+  key_name                    = "pro2"
+
+  root_block_device {
+    volume_size = 96
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = "spark-ec2-instance"
+  }
+}
